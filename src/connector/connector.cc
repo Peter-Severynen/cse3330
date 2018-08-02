88a47ae1a2bf60f2eeadad9c51233a91491c68d1
George Mitchell <georgemaxwellmitchell@outlook.com>
2018-08-02 17:21:00 -0500

    Links with the MySQL Connector lib

--- src/connector/connector.cc
+++ src/connector/connector.cc
@@ -1,1 +1,18 @@
 #include "connector.h"
+
+// Disable warnings about dll-interface
+#ifdef _MSC_VER
+#pragma warning(disable : 4251)
+#endif
+
+#include "cppconn/driver.h"
+
+namespace cse3330 {
+
+    Connector::Connector() {
+
+        driver = get_driver_instance();
+
+    }
+
+} // namespace cse3330
--- src/connector/connector.h
+++ src/connector/connector.h
@@ -4,13 +4,30 @@
 // Holds all database connection behavior
 // Links with the MySQL Connector C++ library dynamically
 
-// TODO: Successfully link with the library
+// Forward declaration of MySQL types
+namespace sql {
+
+    class Driver;
+    class Connection;
+    class Statement;
+    class ResultSet;
+
+}
 
 namespace cse3330 {
 
     class Connector final {
 
+    public:
 
+        Connector();
+
+    private:
+
+        sql::Driver*        driver;
+        sql::Connection*    connection;
+        sql::Statement*     statement;
+        sql::ResultSet*     result_set;
 
     };
 
