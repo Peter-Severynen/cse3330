1e04e64df4bd737658e758afb6bc7190310c62c0
George Mitchell <georgemaxwellmitchell@outlook.com>
2018-08-02 17:01:31 -0500

    Added TODO comments

--- src/connector/connector.h
+++ src/connector/connector.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// Connector object
+// Holds all database connection behavior
+// Links with the MySQL Connector C++ library dynamically
+
+// TODO: Successfully link with the library
+
 namespace cse3330 {
 
     class Connector final {
